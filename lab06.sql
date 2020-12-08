3ba6f94cb4b5e7292b0212988f1f3bbc8ff8b465
nest <nest>
2020-12-08 16:39:29 +0300

    add 01

---
+++ lab06.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (name TEXT, age INT, avatar TEXT);
+
+INSERT INTO employee VALUES ('Apple'	,19	, 'img-apple.png');
+INSERT INTO employee VALUES ('Cherry'	,21	, 'img-cherry.png');
+INSERT INTO employee VALUES ('Orange'	,25	, 'img-orange.png');
+INSERT INTO employee VALUES ('Blueberry'	,30	, 'img-blueberry.png');
+INSERT INTO employee VALUES ('Lemon'	,101	, 'img-lemon.png');
+
